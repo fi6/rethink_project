c7b31faf8f07e6182aea35ecce1898a1d011fa33
Yida Niu <niu.yid@husky.neu.edu>
2019-12-12 19:46:18 -0500

    rebuild again

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/yida/rethink_project/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/yida/rethink_project_pc/devel;/home/yida/rethink_project/devel;/opt/ros/kinetic".split(';') if "/home/yida/rethink_project_pc/devel;/home/yida/rethink_project/devel;/opt/ros/kinetic" != "" else []
--- build/tutorial_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yida/rethink_project/src/tutorial_pkg
-# Build directory: /home/yida/rethink_project/build/tutorial_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
